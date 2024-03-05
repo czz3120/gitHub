53c5270395b09a2f91a6ceaa3b32958291be3be7
czz3120 <110526889+czz3120@users.noreply.github.com>
2024-03-05 17:40:46 +0800

    Update main.py

--- main.py
+++ main.py
@@ -83,3 +83,10 @@ for section in configs.sections():
 
 # 推送消息
 process.send_msg(s_title, s_content)
+if '申购失败' in s_content:
+    title="申购失败通知:"
+elif '申购中' in s_content:
+    title="申购未结束:"
+else:
+    title = "申购成功通知:"
+process.send_msg(title, s_content)
